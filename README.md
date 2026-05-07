f95ddd8caa8baaed77765a542833fd823162a926
truejank <silaserenity@gmail.com>
2026-05-07 13:32:13 -0400

    basic setup

---
+++ README.md
@@ -0,0 +1,8 @@
+**Submitted by:** Rose Smith (superterp)
+**Group Members:** Justin DeBisschop (jdebissc), Spencer Chen ()
+**App Description:** Allow users to find images, songs of an author, and store/retrieve the results.
+**YouTube Video Link:** https://www.youtube.com/MyAmazingAPP
+**APIs:** Free Dictionary API (https://dictionaryapi.dev/), SoundApp (http:notreadlsoundapp.what)
+**Contact Email:**  superterp@notreal.address.what
+**Deployed App Link:** https://notreal.what.terpNel/myApp
+**AI Use:** 1. Claude Code, 2. ChatGPT
---
+++ server.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const portNumber = 7003;
+const bodyParser = require("body-parser");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "templates"));
+
+// const databaseName = "CMSC335DB";
+// const collectionName = "dictionary";
+// const uri = process.env.MONGO_CONNECTION_STRING;
+// const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+
+const DICTIONARY_API = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const TEST_RESULT = [
+{
+    "word": "hello",
+    "phonetic": "həˈləʊ",
+    "phonetics": [
+    {
+        "text": "həˈləʊ",
+        "audio": "//ssl.gstatic.com/dictionary/static/sounds/20200429/hello--_gb_1.mp3"
+    },
+    {
+        "text": "hɛˈləʊ"
+    }
+    ],
+    "origin": "early 19th century: variant of earlier hollo ; related to holla.",
+    "meanings": [
+    {
+        "partOfSpeech": "exclamation",
+        "definitions": [
+        {
+            "definition": "used as a greeting or to begin a phone conversation.",
+            "example": "hello there, Katie!",
+            "synonyms": [],
+            "antonyms": []
+        }
+        ]
+    },
+    {
+        "partOfSpeech": "noun",
+        "definitions": [
+        {
+            "definition": "an utterance of ‘hello’; a greeting.",
+            "example": "she was getting polite nods and hellos from people",
+            "synonyms": [],
+            "antonyms": []
+        }
+        ]
+    },
+    {
+        "partOfSpeech": "verb",
+        "definitions": [
+        {
+            "definition": "say or shout ‘hello’.",
+            "example": "I pressed the phone button and helloed",
+            "synonyms": [],
+            "antonyms": []
+        }
+        ]
+    }
+    ]
+}
+]
+
+app.get("/", async (req, res) => {
+    res.render("index.ejs");
+});
+
+app.get("/dictionary", async (req, res) => {
+    // const {word} = req.body;
+    // const result = await fetch(DICTIONARY_API + word)[0];
+    const result = {...TEST_RESULT[0], valid: true};
+    // const result = {valid: false};
+    res.render("dictionary.ejs", result);
+});
+
+app.listen(portNumber);
+console.log(`Listening on http://localhost:${portNumber}/`);
